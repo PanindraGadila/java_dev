ad4b6e73335eb6a3335d8d04969752bf084f3c20
PanindraGadila <gadilapanindrareddy@gmail.com>
2025-07-22 17:58:40 -0500

    2nd commint i have finished learnin the spring boot testing and slice testing using mockito framework

--- core-spring-labfiles/lab/40-boot-test/src/test/java/accounts/web/AccountControllerBootTests.java
+++ core-spring-labfiles/lab/40-boot-test/src/test/java/accounts/web/AccountControllerBootTests.java
@@ -1,10 +1,27 @@
 package accounts.web;
 
+import accounts.AccountManager;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import rewards.internal.account.Account;
 
+import static org.mockito.BDDMockito.given;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 // TODO-06: Get yourself familiarized with various testing utility classes
 // - Uncomment the import statements below
 //import static org.mockito.ArgumentMatchers.any;
@@ -15,8 +32,21 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 // TODO-07: Replace @ExtendWith(SpringExtension.class) with the following annotation
 // - @WebMvcTest(AccountController.class) // includes @ExtendWith(SpringExtension.class)
-@ExtendWith(SpringExtension.class)
+//@ExtendWith(SpringExtension.class)
+@WebMvcTest(AccountController.class)
 public class AccountControllerBootTests {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private AccountManager accountManager;
+
+//	below fails because the accountManager needs to come from the spring aplicaiton context
+	// here the @mock dont get anything from the sping auto configuration. and it comes from the mockito framework
+//@Mock
+//private AccountManager accountManager;
+
+
 
 	// TODO-08: Autowire MockMvc bean
 
@@ -26,17 +56,19 @@ public class AccountControllerBootTests {
 	// - Uncomment the code and run the test and verify it succeeds
 	@Test
 	public void accountDetails() throws Exception {
-
+		given(accountManager.getAccount(0L)).willReturn(new Account("1234567890", "John Doe"));
 		//given(accountManager.getAccount(0L))
 		//		.willReturn(new Account("1234567890", "John Doe"));
-
+		mockMvc.perform(get("/accounts/0")).andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("name").value("John Doe"))
+				.andExpect(jsonPath("number").value("1234567890"));
 		//mockMvc.perform(get("/accounts/0"))
 		//	   .andExpect(status().isOk())
 		//	   .andExpect(content().contentType(MediaType.APPLICATION_JSON))
 		//	   .andExpect(jsonPath("name").value("John Doe"))
 		//	   .andExpect(jsonPath("number").value("1234567890"));
 
-		//verify(accountManager).getAccount(0L);
+		verify(accountManager).getAccount(0L);
 
 	}
 
@@ -46,16 +78,17 @@ public class AccountControllerBootTests {
 	// - Run the test and verify it succeeds
 	@Test
 	public void accountDetailsFail() throws Exception {
-
+		given(accountManager.getAccount(anyLong())).willThrow(new IllegalArgumentException("No such account with id " + 0L));
 		//given(accountManager.getAccount(any(Long.class)))
 		//		.willThrow(new IllegalArgumentException("No such account with id " + 0L));
 
 		// (Write code here)
+		mockMvc.perform(get("/accounts/9999")).andExpect(status().isNotFound());
 		// - Use mockMvc to perform HTTP Get operation using "/accounts/9999"
         //   as a non-existent account URL
 		// - Verify that the HTTP response status is 404
 
-		//verify(accountManager).getAccount(any(Long.class));
+		verify(accountManager).getAccount(any(Long.class));
 
 	}
 
@@ -66,14 +99,21 @@ public class AccountControllerBootTests {
 	@Test
 	public void createAccount() throws Exception {
 
-		//Account testAccount = new Account("1234512345", "Mary Jones");
-		//testAccount.setEntityId(21L);
+		Account testAccount = new Account("1234512345", "Mary Jones");
+		testAccount.setEntityId(21L);
+
+		given(accountManager.save(any(Account.class))).willReturn(testAccount);
+
 
 		//given(accountManager.save(any(Account.class)))
 		//		.willReturn(testAccount);
 
 		// (Write code here)
 		// Use mockMvc to perform HTTP Post operation to "/accounts"
+
+		mockMvc.perform(post("/accounts/").content(asJsonString(testAccount)).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated())
+				.andExpect(header().string("Location", "http://localhost/accounts/21"));
 		// - Set the request content type to APPLICATION_JSON
 		// - Set the request content with Json string of the "testAccount"
 		//   (Use "asJsonString" method below to convert the "testAccount"
@@ -81,7 +121,7 @@ public class AccountControllerBootTests {
 		// - Verify that the response status is 201
 		// - Verify that the response "Location" header contains "http://localhost/accounts/21"
 
-		//verify(accountManager).save(any(Account.class));
+			verify(accountManager).save(any(Account.class));
 
 	}
 
